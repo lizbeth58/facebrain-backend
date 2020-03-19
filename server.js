6ea75f6d14ef3a8f189d4f35a31bb0dc8f40832c
Elizabeth Ortiz <ortiz.beth58@gmail.com>
2020-03-18 18:28:11 -0700

    first commit

--- server.js
+++ server.js
@@ -1,22 +1,73 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
 
-//! --> route --> response -> this is working
+const database = {
+  users: [
+    {
+      id: "1",
+      name: "liz",
+      email: "liz@gmail.com",
+      password: "liz",
+      entries: "0",
+      joined: new Date()
+    }
+  ]
+};
+
+//* --> route --> response -> this is working
 app.get("/", (req, res) => {
   res.send("this is working");
 });
 
-//! /sign in --> POST -> success/fail
-app.post("/signin", (req, res) => {});
+//* /sign in --> POST -> success/fail
+app.post("/signin", (req, res) => {
+  //get email from request body
+  const email = req.body.email;
 
-//! /register --> POST -> user object
-app.post("/register", (req, res) => {});
+  //get password from request body
+  const password = req.body.password;
 
-//! /profile/:userId --> GET = user object
+  //get user from database
+  //check data
+  //send success or fail
+  if (
+    email === database.users[0].email &&
+    password === database.users[0].password
+  ) {
+    res.json("signed in");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+//TODO /register --> POST -> user object
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+
+  database.users.push({
+    id: "2",
+    name: name,
+    email: email,
+    password: password,
+    entries: "0",
+    joined: new Date()
+  });
+
+  //set entries to 0
+  //calculate date joined
+  //calculate new id
+  //create new user object
+  //populate the object
+  //add to databse users array
+});
+
+//TODO /profile/:userId --> GET = user object
 app.get("/profile/:userId", (req, res) => {});
 
-//! /image --> PUT -> update user
+//TODO /image --> PUT -> update user
 app.put("/image", (req, res) => {});
 
 app.listen(3000, () => {
